2cc447835fc30788b4b475a1746e0181d8992223
Pablo Alessandro Santos Hugen <phugen@redhat.com>
2025-09-05 22:36:41 -0300

    Delete old Direction struct

--- src/Game/Direction.zig
+++
@@ -1,7 +0,0 @@
-const Game = @import("Game");
-const Position = Game.Position;
-
-pub const Up = Position.init(0, -1);
-pub const Down = Position.init(0, 1);
-pub const Left = Position.init(-1, 0);
-pub const Right = Position.init(1, 0);
